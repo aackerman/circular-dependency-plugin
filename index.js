44d61015862f1f562bfd41956e8b765a5800ddec
rqrqrqrq <rqtdfh@gmail.com>
2018-01-30 18:27:25 +0300

    change `exclude` option logic

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ class CircularDependencyPlugin {
           plugin.options.onStart({ compilation });
         }
         for (let module of modules) {
-          if (module.resource === undefined) { continue }
+          const shouldSkip = module.resource === undefined 
+              || plugin.options.exclude.test(module.resource);
+          if (shouldSkip) { continue }
+
           let maybeCyclicalPathsList = this.isCyclic(module, module, {})
           if (maybeCyclicalPathsList) {
             // allow consumers to override all behavior with onDetected
@@ -36,11 +39,6 @@ class CircularDependencyPlugin {
               } catch(err) {
                 compilation.errors.push(err)
               }
-              continue
-            }
-
-            // exclude modules based on regex test
-            if (plugin.options.exclude.test(module.resource)) {
               continue
             }
 
